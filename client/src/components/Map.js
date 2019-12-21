ea2cd8fdba35e97cc48f9e4a88c4602ef903372d
shell <dolwinf@gmail.com>
2019-12-21 19:33:23 +1100

    Added map from mapbox GL and react-map-gl library

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import ReactMapGL, { NavigationControl, Marker, Popup } from "react-map-gl";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPosition] = useState(null);
+
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
+  const getUserPosition = () => {
+    //check window for geolocation that comes from navigator
+    if ("geolocation" in navigator) {
+      //method provided by browser api to get user's current position, which we can then destructure to get lat and long
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        //then set the viewport
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  };
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiZG9sd2luIiwiYSI6ImNrNGY3cW02YjBiOXgza21sejY4ajVuNGUifQ.fhfOUbbpbZ-wOXW9ZTqu7g"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      >
+        {/* Navigation control <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />
+        </div> */}
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,9 +1,16 @@
 import React from "react";
-import withRoot from "../withRoot";
+
 import Header from "../components/Header";
+import Map from "../components/Map";
+import withRoot from "../withRoot";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Map />
+    </>
+  );
 };
 
 export default withRoot(App);
